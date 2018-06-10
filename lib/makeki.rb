92fc31e33e9adace52286792c3b30778cab21af4
Cronwel Noel Irias <noelirias@gmail.com>
2018-06-10 16:53:51 -0400

    update to rack file

--- lib/makeki.rb
+++ lib/makeki.rb
@@ -1,5 +1,9 @@
 require "makeki/version"
 
 module Makeki
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'},
+      ["Hello from Ruby on Makeki"]]
+    end
 end
