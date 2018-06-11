0182603affbffaa26fbbf31eb075ff8e66447767
Cronwel Noel Irias <noelirias@gmail.com>
2018-06-10 23:14:21 -0400

    upgrade to 4

--- lib/makeki.rb
+++ lib/makeki.rb
@@ -3,8 +3,9 @@ require "makeki/version"
 module Makeki
   class Application
     def call(env)
+      'echo debug > debug.txt';
       [200, {'Content-Type' => 'text/html'},
-      ["Hello from Ruby on Makeki"]]
+       ["Hello from Ruby on Makeki"]]
     end
   end
 end
